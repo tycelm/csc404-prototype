3d3590adb04a28fa84c2df379158d97f7391ce3b
Viceu <66227626+Viceu@users.noreply.github.com>
2025-09-13 14:14:55 -0400

    added movement audio

--- Assets/HandControl/PlayerMovement.cs
+++ Assets/HandControl/PlayerMovement.cs
@@ -11,15 +11,17 @@ public class PlayerMovement : MonoBehaviour
     private InputAction dpadAction;
     public Vector3 movement;
 
-    // private void Awake()
-    // {
-    //     rb = GetComponent<Rigidbody>();
-    // }
+    private bool isMoving = false;
+    public AudioSource moveSource; 
+    public AudioSource stopSource;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        // audioSource = GetComponent<AudioSource>();
+
         moveAction = InputSystem.actions.FindAction("Move");
+        // moveAction.Enable();
         dpadAction = InputSystem.actions.FindAction("DpadMove");
     }
 
@@ -34,6 +36,27 @@ public class PlayerMovement : MonoBehaviour
         Vector3 dpadMovement = new Vector3(0, 0, dpadMove.y);
         movement = (stickMovement + dpadMovement) * speed;
         rb.linearVelocity = movement;
+
+
+        bool movingNow = movement.magnitude > 0.01f;
+
+        if (movingNow && !isMoving)
+        {
+            isMoving = true;
+            if (moveSource != null && !moveSource.isPlaying)
+                moveSource.Play();
+        }
+
+        // Trigger sounds when movement stops
+        if (!movingNow && isMoving)
+        {
+            isMoving = false;
+            if (moveSource != null && moveSource.isPlaying)
+                moveSource.Stop();
+
+            if (stopSource != null)
+                stopSource.Play();
+        }
     }
 
     // public void OnMove(InputAction.CallbackContext context)
